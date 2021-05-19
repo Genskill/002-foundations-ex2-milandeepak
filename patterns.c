71d87c543d4e85b0a67e39bbc677ff94373e024b
milandeepak <71485068+milandeepak@users.noreply.github.com>
2021-05-19 23:41:04 +0530

    update patterns.c

--- patterns.c
+++ patterns.c
@@ -6,7 +6,17 @@ int main(void)
 {
   int p= get_int("Enter type of pattern (1 or 2)");
   if(p==1){
+    int n= get_int("enter no:");
+  for(int i=n; i>=1; i--){
+    for(int j=1; j<=i; j++){
+       printf("#");
+     }
     
+   printf("\n");
+  }
+  
+ }
+  else if(p==2){
   int n= get_int("enter no:");
   for(int i=0; i<=n; i++){
     for(int j=1; j<=n-i; j++){
@@ -18,16 +28,3 @@ int main(void)
    printf("\n");
   }
  }
-  else if(p==2){
-  int n= get_int("enter no:");
-  for(int i=n; i>=1; i--){
-    for(int j=1; j<=i; j++){
-       printf("#");
-     }
-    
-   printf("\n");
-  }
-  }
-  
-
-}
