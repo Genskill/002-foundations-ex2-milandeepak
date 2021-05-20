516ba7a503400b60e5d57fb86a4ac3b18eadbf87
milandeepak <71485068+milandeepak@users.noreply.github.com>
2021-05-20 14:36:49 +0530

    Updatepatterns.c

--- patterns.c
+++ patterns.c
@@ -1,3 +1,4 @@
+
 #include<stdio.h>
 #include<cs50.h>
 
@@ -18,7 +19,7 @@ int main(void)
 
   else if(p==2){
   for(int i=1; i<=n; i++){
-    for(int j=0; j<=n-i; j++){
+    for(int j=1; j<=n-i; j++){
        printf(" ");
      }
     for(int k=1; k<=i;k++){
